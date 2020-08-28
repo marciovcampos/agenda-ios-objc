3ac2719d8200c54170b3235fbed51075ec7d248a
Márcio Vinícius <marcio.campos@outlook.com>
2020-08-28 14:52:02 -0300

    chamando tela FormContato pelo botão add

--- AgendaDeContatos/ListaContatosViewController.m
+++ AgendaDeContatos/ListaContatosViewController.m
@@ -7,17 +7,24 @@
 //
 
 #import "ListaContatosViewController.h"
+#import "ViewController.h"
 
 @implementation ListaContatosViewController
 
 -(ListaContatosViewController *) init {
     self = [super init];
     
-    UIBarButtonItem *botaoForm = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:(UIBarButtonSystemItemAdd) target:self action:nil];
+    UIBarButtonItem *botaoForm = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:(UIBarButtonSystemItemAdd) target:self action:@selector(exibeFormulario)];
     
     self.navigationItem.rightBarButtonItem = botaoForm;
     
     return self;
+}
+
+-(void) exibeFormulario {
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    UIViewController *form = [storyboard instantiateViewControllerWithIdentifier:@"Form-Contato"];
+    [self.navigationController pushViewController:form animated:true];
 }
 
 @end
