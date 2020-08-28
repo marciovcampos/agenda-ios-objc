386c69752a7e223ccbce7fb4f244412273cccde0
Márcio Vinícius <marcio.campos@outlook.com>
2020-08-28 14:34:34 -0300

    add botaoForm

--- AgendaDeContatos/ListaContatosViewController.m
+++ AgendaDeContatos/ListaContatosViewController.m
@@ -10,4 +10,14 @@
 
 @implementation ListaContatosViewController
 
+-(ListaContatosViewController *) init {
+    self = [super init];
+    
+    UIBarButtonItem *botaoForm = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:(UIBarButtonSystemItemAdd) target:self action:nil];
+    
+    self.navigationItem.rightBarButtonItem = botaoForm;
+    
+    return self;
+}
+
 @end
