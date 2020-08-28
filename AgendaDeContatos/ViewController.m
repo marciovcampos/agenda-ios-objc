c77a6097b3f9d5db5712ec81235777eebcffd855
Márcio Vinícius <marcio.campos@outlook.com>
2020-08-28 11:16:51 -0300

    acessando a propriedade text

--- AgendaDeContatos/ViewController.m
+++ AgendaDeContatos/ViewController.m
@@ -16,11 +16,11 @@
 
 - (IBAction) adiciona {
     
-    NSString *nome = [self.nome text];
-    NSString *endereco = [self.endereco text];
-    NSString *email = [self.email text];
-    NSString *telefone = [self.telefone text];
-    NSString *site = [self.site text];
+    NSString *nome = self.nome.text;
+    NSString *endereco = self.endereco.text;
+    NSString *email = self.email.text;
+    NSString *telefone = self.telefone.text;
+    NSString *site = self.site.text;
     NSLog(@"Dados do Contato %@ - %@ - %@ - %@ - %@", nome, endereco, email, telefone, site);
     
 }
